335bfcfef00fbc6ec6ae119481dc12f0a2eacebc
Tim Holmes <Tim.Holmes@sdl.usu.edu>
2016-08-30 15:27:49 -0600

    Step 3 complete

--- StockDog/app/scripts/app.js
+++ StockDog/app/scripts/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * @ngdoc overview
@@ -9,18 +9,26 @@
  * Main module of the application.
  */
 angular
-  .module('stockDogApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'mgcrea.ngStrap'
+  .module("stockDogApp", [
+    "ngAnimate",
+    "ngCookies",
+    "ngResource",
+    "ngRoute",
+    "ngSanitize",
+    "ngTouch",
+    "mgcrea.ngStrap"
   ])
   .config(function ($routeProvider) {
     $routeProvider
+      .when("/dashboard", {
+        templateUrl: "views/dashboard.html",
+        controller: "DashboardCtrl"
+      })
+      .when("/watchlist/:listId", {
+        templateUrl: "views/watchlist.html",
+        controller: "WatchlistCtrl"
+      })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: "/dashboard"
       });
   });
