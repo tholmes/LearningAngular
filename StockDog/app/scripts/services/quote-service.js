d4be6c4f9925ff74b8193499c03e5b9c45efeddd
Tim Holmes <Tim.Holmes@sdl.usu.edu>
2016-09-03 08:23:15 -0600

    added quote-service and renamed companyservice to company-service

---
+++ StockDog/app/scripts/services/quote-service.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * @ngdoc service
+ * @name stockDogApp.QuoteService
+ * @description
+ * # QuoteService
+ * Service in the stockDogApp.
+ */
+angular.module("stockDogApp")
+  .service("QuoteService", function QuoteService() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
